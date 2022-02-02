33b06c5276ce9d715f1d9114e14c5b55f041ee8a
Mareli95 <mareli.rubio.izaguirre@gmail.com>
2022-02-02 11:41:17 -0600

    Practica de Licuadora con css, html y js

---
+++ js/motor.js
@@ -0,0 +1,32 @@
+// console.log('123');
+'use strict';
+let estadoLicuadora = 'apagada';
+
+let licuadora = document.getElementById('blender');
+let sonidoLicuadora = document.getElementById('blender-sound');
+var botonLicuadora = document.getElementById('blender-button-sound');
+function controlarLicuadora() {
+	if (estadoLicuadora == 'apagada') {
+		estadoLicuadora = 'encendido';
+		sonido();
+		licuadora.classList.add('active');
+
+		console.log('me prendiste');
+	} else {
+		estadoLicuadora = 'apagada';
+		sonido();
+		licuadora.classList.remove('active');
+		console.log('me apagaste');
+	}
+}
+
+function sonido() {
+	if (sonidoLicuadora.paused) {
+		botonLicuadora.play();
+		sonidoLicuadora.play();
+	} else {
+		botonLicuadora.play();
+		sonidoLicuadora.pause();
+		sonidoLicuadora.currentTime = 0;
+	}
+}
